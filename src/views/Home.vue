15d6c9615caaf740eb6377ad246c6335640d32c4
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-04-15 13:58:28 +0900

    login pages

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <amplify-sign-out></amplify-sign-out>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
